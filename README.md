090e0af660795caa8f6da8d0e24f075e3edcfe35
flajr <flajr.marko@gmail.com>
2015-09-25 20:12:39 +0200

    Used awk to print last line without \n

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# waw-backup 1.0.0
+# waw-backup 4.0.0
 What and Where Backup. Easy as possible backup, with file what.txt (what to backup) and where.txt (where to backup).
 
 what.txt mean paths to directories/files what you want to backup.
@@ -9,7 +9,7 @@ where.txt mean paths to directories where paths from what.txt will be copied.
 sudo chmod +x waw-backup.sh
 ./waw-backup.sh
 ```
-After this command program dumped 2 files in his directory (what.txt and where.txt).
+After this command program dumped 2 files in directory (what.txt and where.txt).
 Now you can simple add paths (preferable FULL PATHS) in that files.
 
 Then try use argument -t to see what will be done. 
@@ -23,47 +23,56 @@ If you want to proceed backup use argument -b
 
 # Simulation
 ```bash
-Simulation Backup will proceed!
+marek@1015CX:~/Sync/waw-backup$ ./waw-backup.sh -t
+Just Simulation
 
-WHAT 1. dir 
-/home/test/Documents/ #from what.txt file, dir mean directory
-WHERE
-/home/test/Sync/ #from where.txt file
-/home/test/Desktop/
-/home/test/Documents/
-/folder1 doesn't exist!
-/folder two doesn't exist!
-/home/test/tput_test.sh is not folder #not folder so you can't backup there
+/home/marek/git repos [WHAT] [DIR]
+/home/marek/backup_folder [WHERE]
+/home/marek/backup_folder2 [WHERE] wasn't created (use -y)
+/home/marek/backup_folder 3 [WHERE] wasn't created (use -y)
 
-WHAT 2. file #file mean regular file
-/home/test/Sync/Sync/pineapples_final2.png
-WHERE
-/home/test/Sync/
-/home/test/Desktop/
-/home/test/Documents/
-/test space doesn't exist!
-/something doesn't exist!
-/home/test/tput_test.sh is not folder
+/home/marek/bookmarks.html [WHAT] [FILE]
+/home/marek/backup_folder [WHERE]
+/home/marek/backup_folder2 [WHERE] wasn't created (use -y)
+/home/marek/backup_folder 3 [WHERE] wasn't created (use -y)
 
-WHAT 3. /home/test/test_folder/something doesn't exist!
-
-WHAT 4. /home/find_me doesn't exist!
+/home/marek/something.txt [WHAT] [LINK]
+/home/marek/backup_folder [WHERE]
+/home/marek/backup_folder2 [WHERE] wasn't created (use -y)
+/home/marek/backup_folder 3 [WHERE] wasn't created (use -y)
 ```
 
 # Backup
 ```bash
+marek@1015CX:~/Sync/waw-backup$ ./waw-backup.sh -by #backup and create folders
 Are you sure to proceed backup? Try argument -t first. (Yy/Nn) : y
-Backup will proceed!
 
-WHAT 1. file
-/home/test/dnt-ver.9/additional_functions.sh
-WHERE
-/home/test_folder/test doesn't exist!
-/home/test/test_two > file copied!
+/home/marek/git repos [WHAT] [DIR]
+/home/marek/backup_folder [WHERE] [COPYING] [OK]
+/home/marek/backup_folder2 [WHERE] [CREATED] [COPYING] [OK]
+/home/marek/backup_folder 3 [WHERE] [CREATED] [COPYING] [OK]
 
-WHAT 2. dir
-/home/test/dnt-ver.9/dialog_tcpdump-rpm
-WHERE
-/home/test_folder/test doesn't exist!
-/home/test/test_two > dir copied!
+/home/marek/bookmarks.html [WHAT] [FILE]
+/home/marek/backup_folder [WHERE] [COPYING] [OK]
+/home/marek/backup_folder2 [WHERE] [CREATED] [COPYING] [OK]
+/home/marek/backup_folder 3 [WHERE] [CREATED] [COPYING] [OK]
+
+/home/marek/something.txt [WHAT] [LINK]
+/home/marek/backup_folder [WHERE] [COPYING] [OK]
+/home/marek/backup_folder2 [WHERE] [CREATED] [COPYING] [OK]
+/home/marek/backup_folder 3 [WHERE] [CREATED] [COPYING] [OK]
+```
+# Configure
+```bash
+marek@1015CX:~/Sync/waw-backup$ ./waw-backup.sh -c
+Path to file/folder you want to backup (no input = continue)
+WHAT > /home/marek/git repos
+WHAT > /home/marek/bookmarks.html
+WHAT > /home/marek/something.txt
+WHAT > /home/marek/Sync/waw-backup
+
+Path to folder, where you want to create your backups (no input = continue)
+WHERE > /home/marek/backup_folder
+WHERE > /home/marek/backup_folder2
+WHERE > /home/marek/backup_folder 3
 ```
--- waw-backup.sh
+++ waw-backup.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 #waw-backup
-waw_backup_version="1.0.0"
+if [[ $(id -u) -eq 0 ]]; then
+	echo "At this state of program, DO NOT RUN AS ROOT!"
+	exit
+fi
+
+waw_backup_version="4.0.0"
+
+function usage()
+{
+	echo "waw-backup version $waw_backup_version"
+	echo
+	echo "Usage: waw-backup [ARGUMENTS]"
+	echo "       -b   | proceed backup"
+	echo "       -c   | configure entries in $what and $where files"
+	echo "       -s   | show what and where files"
+	echo "       -t   | test files and folders, emulated backup"
+	echo "       -y   | try create folders which do not exist (if is argument alone program use -yt instead)"
+	echo "what = What to backup; where = Where to backup"
+	echo
+	echo "Example: waw-backup -ty #test files and folders and create unexist (where) dirs"
+	echo "         waw-backup -by #proceed backup and create where dirs, if don't exist"
+	usage="hide_usage"
+	exit
+}
 
 #default configuration
 function default_config()
@@ -26,6 +49,15 @@ function config()
 { 
 		echo "Path to file/folder you want to backup (no input = continue)"
 		what_entry="proceed"
+		while read line; do
+			if [[ -z $line ]]; then
+				continue
+				
+			else
+				echo "WHAT > $line"
+				
+			fi
+		done < <(cat $what |awk '{print $1}')
 		until [[ -z $what_entry ]]; do	
 			read -e -p "WHAT > " what_entry
 				if [[ -n $what_entry ]]; then
@@ -39,6 +71,15 @@ function config()
 		echo
 		echo "Path to folder, where you want to create your backups (no input = continue)"
 		where_entry="proceed"
+		while read line; do
+			if [[ -z $line ]]; then
+				continue
+
+			else
+				echo "WHERE > $line"
+
+			fi
+		done< <(cat $where |awk '{print $1}')
 		until [[ -z $where_entry ]]; do		
 			read -e -p "WHERE > " where_entry
 				if [[ -n $where_entry ]]; then
@@ -58,13 +99,16 @@ function copy_dir()
 		echo
 	
 	else	
-		cp -R "$what_read" "$where_read"
-		if [[ $? -eq 0 ]]; then
-			echo " > dir copied!"
-		else
-			echo " > ERROR!"
-		fi
-	
+		printf " [COPYING]"
+		cp -R -- "$what_read" "$where_read" 2>> log_file.txt
+		local exit_status=$?
+			
+			if [[ $exit_status -eq 0 ]]; then
+				printf " [OK]\n"
+			else
+				printf " [ERROR] [$exit_status]\n"
+			
+			fi
 	fi
 }
 
@@ -74,136 +118,216 @@ function copy_file()
 		echo
 	
 	else
-		cp "$what_read" "$where_read"
-		if [[ $? -eq 0 ]]; then
-			echo " > file copied!"
-		else
-			echo " > ERROR!"
-		
-		fi
+		printf " [COPYING]"
+		cp -- "$what_read" "$where_read" 2>> log_file.txt
+		local exit_status=$?
+			
+			if [[ $exit_status -eq 0 ]]; then
+				printf " [OK]\n"
+			else
+				printf " [ERROR] [$exit_status]\n"
+			
+			fi
 	fi
 }
 
 function backup()
 {
-if [[ ! -r $what ]]; then
-				echo "What to backup in file $what - No file found, or is unreadable."
-				exit
+	local prompt until_loop_where="yes" until_loop 
+		if [[ ! -v prompt_where ]]; then
+			prompt_where="n" #default, do not create dirs
+		
+		fi
 
-			elif [[ -z $(cat $what) ]]; then
-				echo "What to backup in file $what - No path specified, nothing to backup."
-				exit
+	if [[ ! -r $what ]]; then
+		echo "No $what file found, or is unreadable"
+		exit 1 #no file found, or unreadable
 
-
-			fi
-
-			if [[ ! -r $where ]]; then
-				echo "Where save backup in file $where - No file found, or is unreadable."
-				exit
-
-			elif [[ -z $(cat $where) ]]; then
-				echo "Where save backup in file $where - No path specified, don't know where save backup."
-				exit
-
-			fi
-
-				until [[ $until_loop == "no" ]]; do
-					if [[ $simulation == "yes" ]]; then
-						echo -n "Simulation "
-						prompt="y"
-
-					else
-						read -p "Are you sure to proceed backup? Try argument -t first. (Yy/Nn) : " prompt
-						
-					fi
-
-						
-						if [[ $prompt =~ Y|y ]]; then
-							echo "Backup will proceed!"
-							count=0
-								while read LINE; do
-								if [[ $LINE != "" ]]; then
-								
-								((count++))
-								echo
-								echo -n "WHAT $count. "
-									what_read=$LINE
-									where_printout="yes"	
-									copy=
-
-										if [[ -e $what_read ]]; then
-											if [[ -d $what_read ]]; then
-												echo "dir"
-												echo "$what_read"
-												copy="dir"
-
-											elif [[ -r $what_read ]]; then
-												echo "file"
-												echo "$what_read"
-												copy="file"
-
-											elif [[ -L $what_read ]]; then
-												echo "link"
-												echo "$what_read"
-												copy="file"
+	elif [[ -z $(cat $what) ]]; then
+		echo "No paths defined in $what file"
+		exit 2 #no paths specified
 
 
-											else
-												echo "Unknown file/folder!"
-												where_printout="no"
+	fi
 
-											fi
-										
-										else
-											echo "$what_read doesn't exist!"
-											where_printout="no"
+	if [[ ! -r $where ]]; then
+		echo "No $where file found, or is unreadable"
+		echo "Will use default ~/waw-backup-d"
+		echo ~/"waw-backup-d" > /tmp/temp-where-file.txt #use file because of while reading from file
+		where="/tmp/temp-where-file.txt"
 
-										fi
+	elif [[ -z $(cat $where) ]]; then
+		echo "No paths defined in $where file"
+		echo "Will use default ~/waw-backup-d"
+		echo ~/"waw-backup-d" > /tmp/temp-where-file.txt #use file because of while reading from file
+		where="/tmp/temp-where-file.txt"
 
-									if [[ $where_printout == "yes" ]]; then
-									echo "WHERE"	
-									while read LINE; do
-									if [[ $LINE != "" ]]; then
-										where_read=$LINE
-											if [[ -e $where_read ]]; then
-												if [[ -d $where_read ]]; then
-													echo -n "$where_read"
-														if [[ $copy == "dir" ]]; then
-															copy_dir
-														
-														else
-															copy_file
-														
-														fi
+	fi
 
-												else
-													echo "$where_read is not folder"
+	until [[ $until_loop == "no" ]]; do
+		if [[ $simulation == "yes" ]]; then
+			echo "Just Simulation"
+			prompt="y"
 
-												fi
+		else
+			read -p "Are you sure to proceed backup? Try argument -t first. (Yy/Nn) : " prompt
+			
+		fi
 
-											else
-												echo "$where_read doesn't exist!"
-												
-											fi
-									fi
-									done < <(cat $where)
-									
-									fi
-								
-								fi
-								done < <(cat $what)
+			
+			if [[ $prompt =~ Y|y ]]; then
+				if [[ -s log_file.txt ]]; then
+					cat /dev/null > log_file.txt
+				fi
 
-							until_loop="no"
-						elif [[ $prompt =~ N|n ]]; then
-							echo "Backup won't proceed!"
-							until_loop="no"
+				count=0
+				while read LINE; do
+				if [[ -z $LINE ]]; then
+					continue
+
+				else
+					where_read="$LINE"
+						if [[ -e $where_read ]]; then
+							if [[ -d $where_read ]]; then
+								where_read_status[$count]=1 #is dir
+
+							else
+								where_read_status[$count]=2 #is not dir
+
+							fi
+
+						where_read_var[$count]="$where_read"
+						let count++
+
 						else
-							until_loop="yes"
-						
-						
+							while [[ $until_loop_where == "yes" ]]; do
+									if [[ $prompt_where == "y" ]]; then
+										mkdir "$where_read"
+											if [[ $? -ne 0 ]]; then
+												where_read_status[$count]=3 #can't create folder
+											
+											else
+												where_read_status[$count]=4 #created folder
+
+											fi
+
+										until_loop_where="no"
+
+									elif [[ $prompt_where == "n" ]]; then
+										where_read_status[$count]=5 #if doesn't exist, do not create
+										until_loop_where="no"
+
+									fi
+								
+							done
+
+							where_read_var[$count]="$where_read"
+							let count++
+							until_loop_where="yes"
+
 						fi
+				fi
+				done < <(cat $where |awk '{print $1}')
+
+				let count--
+				while read LINE; do
+				if [[ -z $LINE ]]; then
+					continue
+
+				else
+				echo
+				what_read="$LINE"
+				where_printout="yes"	
+				copy=
+
+					if [[ -e $what_read ]]; then
+					printf "$what_read [WHAT]"
+						if [[ -d $what_read ]]; then
+							printf " [DIR]\n"
+							copy="dir"
+
+						elif [[ -L $what_read ]]; then
+							printf " [LINK]\n"
+							copy="file"
+
+						elif [[ -r $what_read ]]; then
+							printf " [FILE]\n"
+							copy="file"
+
+
+						else
+							echo "Unknown file/folder!"
+							where_printout="no"
+
+						fi
+					
+					else
+						echo "$what_read doesn't exist! [WHAT]"
+						where_printout="no"
+
+					fi
+					
+					if [[ $where_printout == "yes" ]]; then
+						
+						for i in `seq 0 $count`; do
+						where_read="${where_read_var[$i]}"
+								case ${where_read_status[$i]} in
+									
+									1)
+										printf "$where_read [WHERE]" #do not new line [COPYING
+											if [[ $copy == "dir" ]]; then
+												copy_dir
+											
+											else
+												copy_file
+											
+											fi
+									;;
+									2)
+										printf "$where_read [WHERE] is not folder\n"
+									;;
+									3)
+										printf "$where_read [WHERE] can't be create\n"
+									;;
+									4)
+										printf "$where_read [WHERE] [CREATED]"
+											if [[ $copy == "dir" ]]; then
+												copy_dir
+											
+											else
+												copy_file
+											
+											fi
+									;;
+									5)
+										printf "$where_read [WHERE] wasn't created (use -y)\n"
+									;;
+									*)
+										printf "${where_read_status[$i]} [WHERE] [ERROR]\n"	
+									;;
+								esac
+
+						done
+					
+					fi
 				
-				done
+				fi
+				done < <(cat $what |awk '{print $1}')
+
+				until_loop="no"
+			elif [[ $prompt =~ N|n ]]; then
+				until_loop="no"
+			else
+				until_loop="yes"
+			
+			
+			fi
+	
+			if [[ -s log_file.txt ]]; then
+				printf "\nReported errors: \n"
+				cat log_file.txt
+			fi
+	done
 }
 
 #SCRIPT START############################################################
@@ -211,7 +335,7 @@ default_config
 
 ####################################
 #show program usage if no argument given
-if [[ -z $1 ]]; then
+if [[ $# -eq 0 ]]; then
 	usage="show"
 
 else
@@ -219,7 +343,7 @@ else
 
 fi
 
-until [[ -z "$1" && $usage == "hide_usage" ]]; do
+until [[ -z $1 && $usage == "hide_usage" ]]; do
 	case $1 in
 		-c) #run config-menu
 			config
@@ -230,16 +354,21 @@ until [[ -z "$1" && $usage == "hide_usage" ]]; do
 			backup
 			exit		
 			;;
+		-by|yb)
+			prompt_where="y"
+			backup
+			exit
+			;;
 
 		-s)
 			echo "what to backup in $what :"
 			echo
-			while read LINE; do
+			while read -r LINE; do
 				if [[ -n $LINE ]]; then
 					echo "$LINE"
 					
 				fi
-			done < <(cat $what)
+			done < <(cat $what |awk '{print $1}')
 			echo
 			echo "where to backup in $where :"
 			echo 
@@ -248,7 +377,7 @@ until [[ -z "$1" && $usage == "hide_usage" ]]; do
 					echo "$LINE"
 					
 				fi
-			done < <(cat $where)
+			done < <(cat $where |awk '{print $1}')
 			exit
 			;;
 
@@ -258,17 +387,15 @@ until [[ -z "$1" && $usage == "hide_usage" ]]; do
 			exit
 			;;
 
-		*)
-			echo "waw-backup version $waw_backup_version"
-			echo
-			echo "Usage: Run with one argument only."
-			echo "       -b    | proceed backup"
-			echo "       -c    | configure entries in $what and $where files"
-			echo "       -s    | show what and where files"
-			echo "       -t    | test files and folders, emulated backup"
-			echo "what = What to backup; where = Where to backup"
-			usage="hide_usage"
+		-ty|-yt|-y)
+			simulation="yes"
+			prompt_where="y"
+			backup
 			exit
+			;;
+
+		*)
+			usage
 			;;
 
 	
